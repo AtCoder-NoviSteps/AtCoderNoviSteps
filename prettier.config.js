190319d866cd18dae6479bac3c3a3a2560d5f59d
Kato Hiroki <k.hiro1818@gmail.com>
2023-08-30 00:06:38 +0000

    :wrench: Add and update packages (#3)

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+// See:
+// https://prettier.io/docs/en/configuration.html
+// https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/207
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+};
