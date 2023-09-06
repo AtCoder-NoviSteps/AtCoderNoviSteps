0a6fe83a97b6b5a351ab36ce555789b9573a5308
Kato Hiroki <k.hiro1818@gmail.com>
2023-09-06 07:19:58 +0000

    ♻️ Refactoring (#54)

--- src/lib/services/tasks.ts
+++ src/lib/services/tasks.ts
@@ -1,6 +1,8 @@
 // TODO: Enable to fetch data from the database via API.
+import { error } from '@sveltejs/kit';
 import { tasks } from '$lib/server/sample_data';
 import type { Task, Tasks } from '$lib/types/task';
+import { NOT_FOUND } from '$lib/constants/http-response-status-codes';
 
 export async function getTasks(): Promise<Tasks> {
   return tasks.map((task: Task) => ({
@@ -11,4 +13,12 @@ export async function getTasks(): Promise<Tasks> {
     user_id: task.user_id,
     submission_result: task.submission_result,
   }));
+}
+
+export async function getTask(slug: string): Promise<Task> {
+  const task = tasks.find((task) => task.id === slug);
+
+  if (!task) throw error(NOT_FOUND);
+
+  return task;
 }
--- src/routes/problems/[slug]/+page.server.ts
+++ src/routes/problems/[slug]/+page.server.ts
@@ -1,12 +1,8 @@
-import { error } from '@sveltejs/kit';
-import { tasks } from '../sample_data';
-import { NOT_FOUND } from '$lib/constants/http-response-status-codes';
+import * as tasks from '$lib/services/tasks';
 
-// TODO: Enable to fetch data from the database via API.
-export function load({ params }) {
-  const task = tasks.find((task) => task.id === params.slug);
-
-  if (!task) throw error(NOT_FOUND);
+// TODO: ユーザを識別できるようにする。
+export async function load({ params }) {
+  const task = await tasks.getTask(params.slug as string);
 
   return { task };
 }
