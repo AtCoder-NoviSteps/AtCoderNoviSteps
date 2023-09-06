ebf8fca59f37fec681fddd5aa28506c15106e8cb
Kato Hiroki <k.hiro1818@gmail.com>
2023-09-06 05:50:15 +0000

    ♻️ Refactoring (#52)

--- src/routes/problems/+page.server.ts
+++ src/routes/problems/+page.server.ts
@@ -1,15 +1,11 @@
-// TODO: Enable to fetch data from the database via API.
-import { tasks } from './sample_data';
+import { getTasks } from '$lib/services/tasks';
+import type { Tasks } from '$lib/types/task';
 
-export function load() {
+// TODO: ユーザを識別できるようにする。
+export async function load() {
+  const tasks: Tasks = await getTasks();
+
   return {
-    tasks: tasks.map((task) => ({
-      contest_id: task.contest_id,
-      id: task.id,
-      title: task.title,
-      grade: task.grade,
-      user_id: task.user_id,
-      submission_result: task.submission_result,
-    })),
+    tasks: tasks,
   };
 }
