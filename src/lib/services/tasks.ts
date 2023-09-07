44424cd50b997eeeaeb7e005bb4ca384f6e34b0b
Kato Hiroki <k.hiro1818@gmail.com>
2023-09-07 08:13:55 +0000

    ♻️ Use dummy db (#56)

--- src/lib/services/tasks.ts
+++ src/lib/services/tasks.ts
@@ -1,11 +1,16 @@
 import { error } from '@sveltejs/kit';
+import db from '$lib/server/database';
 import { tasks } from '$lib/server/sample_data';
 import type { Task, Tasks } from '$lib/types/task';
 import { NOT_FOUND } from '$lib/constants/http-response-status-codes';
 
 // TODO: Enable to fetch data from the database.
 export async function getTasks(): Promise<Tasks> {
-  return tasks.map((task: Task) => ({
+  // FIXME: 問題そのものとユーザの回答状況を分離 + ここでデータをO(N + M)で合成
+
+  // TODO: useIdを動的に変更できるようにする。
+  const userId = 'hogehoge';
+  const sampleTasks = tasks.map((task: Task) => ({
     contest_id: task.contest_id,
     id: task.id,
     title: task.title,
@@ -13,12 +18,20 @@ export async function getTasks(): Promise<Tasks> {
     user_id: task.user_id,
     submission_result: task.submission_result,
   }));
+
+  if (!db.has(userId)) {
+    db.set(userId, sampleTasks);
+  }
+
+  return Array.from(db.get(userId).values());
 }
 
 export async function getTask(slug: string): Promise<Task> {
-  const task = tasks.find((task) => task.id === slug);
+  // TODO: useIdを動的に変更できるようにする。
+  const userId = 'hogehoge';
+  const task: Task = db.get(userId).find((task: Task) => task.id === slug);
 
-  if (!task) throw error(NOT_FOUND);
+  if (!task) throw error(NOT_FOUND, `問題 ${slug} は見つかりませんでした。`);
 
   return task;
 }
