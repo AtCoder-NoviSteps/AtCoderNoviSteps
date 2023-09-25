6540e1b95f9b675f13519ae16ef34fa54e6799d6
Kato Hiroki <k.hiro1818@gmail.com>
2023-09-25 12:47:06 +0000

    :sparkles: Allow only login users to view problems (#5)

--- src/routes/problems/+page.server.ts
+++ src/routes/problems/+page.server.ts
@@ -1,8 +1,15 @@
 import * as crud from '$lib/services/task_results';
 import type { TaskResults } from '$lib/types/task';
+import { redirect } from '@sveltejs/kit';
 
 // TODO: ユーザを識別できるようにする。
-export async function load() {
+export async function load({ locals }) {
+  const session = await locals.auth.validate();
+
+  if (!session) {
+    throw redirect(302, '/login');
+  }
+
   return {
     taskResults: (await crud.getTaskResults()) as TaskResults,
   };
