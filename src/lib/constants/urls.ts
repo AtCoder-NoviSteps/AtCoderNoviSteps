3cf2119ad31afee377fd25a454a2ed8b2e35aa59
Kato Hiroki <k.hiro1818@gmail.com>
2023-09-27 23:46:01 +0000

    :books: Set product name (#33)

--- src/lib/constants/urls.ts
+++ src/lib/constants/urls.ts
@@ -4,4 +4,4 @@ export const ATCODER_BASE_CONTEST_URL: string = `${ATCODER_BASE_URL}/contests`;
 
 export const ATCODER_PROBLEMS_URL: string = 'https://kenkoooo.com/atcoder/#/';
 
-export const GITHUB_URL: string = 'https://github.com/';
+export const GITHUB_URL: string = 'https://github.com/KATO-Hiro/AtCoderNovisteps';
