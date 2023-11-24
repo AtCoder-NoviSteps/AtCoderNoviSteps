741d51f75eb99fe42ccca0fabb2a86ec22acbd06
Kato Hiroki <k.hiro1818@gmail.com>
2023-11-24 13:16:43 +0000

    :pencil2: Fix typo (#141)

--- src/lib/constants/product-info.ts
+++ src/lib/constants/product-info.ts
@@ -1,4 +1,4 @@
-export const PRODUCT_NAME: string = 'AtCoder Novisteps';
+export const PRODUCT_NAME: string = 'AtCoder NoviSteps';
 
 export const PRODUCT_URL: string = 'https://atcoder-novisteps.vercel.app/';
 
