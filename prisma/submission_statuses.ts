ca4bbfeb9bfec8d3e26febfc25a19e076186977a
Kato Hiroki <k.hiro1818@gmail.com>
2023-12-13 06:49:56 +0000

    :art: Use colors of AtCoder submission statuses (#321)

--- prisma/submission_statuses.ts
+++ prisma/submission_statuses.ts
@@ -4,21 +4,21 @@ export const submission_statuses = [
     label_name: 'No Sub',
     image_path: 'ns.png',
     is_AC: false,
-    button_color: '#FFBC42',
+    button_color: '#FFFFFF',
   },
   {
     id: '2',
     label_name: 'WA',
     image_path: 'wa.png',
     is_AC: false,
-    button_color: '#FFBC42',
+    button_color: '#F0AD4E',
   },
   {
     id: '3',
     label_name: 'AC',
     image_path: 'ac.png',
     is_AC: true,
-    button_color: '#218380',
+    button_color: '#5CB85C',
   },
 ];
 
