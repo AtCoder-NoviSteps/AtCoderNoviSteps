c18973ee12e6055e4e9462a60c4d5febce5584e9
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2023-12-22 13:41:46 +0900

    revert to 6ab4d2c  (#321)

--- src/lib/services/answers.ts
+++ src/lib/services/answers.ts
@@ -7,18 +7,19 @@
 // import { PrismaClient } from '@prisma/client';
 // prisma.taskAnswer.createが実行できれば、defineTaskAnswerFactoryをインポートしなくて済むはずです。
 // また、initializeは、内部メソッドを直接インポートすることで対処できそうです。
-//import { initialize } from '@quramy/prisma-fabbrica/lib/internal';
-///import { defineTaskAnswerFactory } from '../../__generated__/fabbrica';
+
+import { initialize } from '@quramy/prisma-fabbrica/lib/internal';
+//import { initialize, defineTaskAnswerFactory } from '../../__generated__/fabbrica';
 
 //import { default as prisma } from '$lib/server/database';
 //const prisma = new PrismaClient();
-import client from '$lib/server/database';
+import { default as prisma } from '$lib/server/database';
 import type { TaskAnswer } from '@prisma/client';
-//initialize({ client });
+initialize({ prisma });
 
 //あとで入れ替える
 export async function getAnswers(user_id: string) {
-  const answers_from_db = client.taskAnswer.findMany({
+  const answers_from_db = prisma.taskAnswer.findMany({
     where: {
       user_id: user_id,
     },
@@ -32,7 +33,7 @@ export async function getAnswers(user_id: string) {
 }
 
 export async function getAnswer(task_id: string, user_id: string) {
-  const answers_from_db = await client.taskAnswer.findMany({
+  const answers_from_db = await prisma.taskAnswer.findMany({
     where: {
       AND: [{ task_id: task_id }, { user_id: user_id }],
     },
@@ -66,7 +67,7 @@ export async function createAnswer(task_id: string, user_id: string, status_id: 
   };
 
   //const taskAnswerFactory = defineTaskAnswerFactory();
-  const taskAnswer = await client.taskAnswer.create({
+  const taskAnswer = await prisma.taskAnswer.create({
     data: taskanswerInput,
   });
   //const taskAnser = await taskAnswerFactory.create({
