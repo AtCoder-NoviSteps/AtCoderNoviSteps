6ce00e8ec537bc7ef8ad3378d3f5a01427313d03
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2024-01-06 14:34:46 +0900

    fix bug

--- src/routes/(admin)/tasks/[task_id]/+page.svelte
+++ src/routes/(admin)/tasks/[task_id]/+page.svelte
@@ -2,9 +2,8 @@
   import TaskForm from '$lib/components/TaskForm.svelte';
   export let data;
   let task = data.task;
-  let isAdmin = data.isAdmin;
 </script>
 
 <div class="container mx-auto w-1/4">
-  <TaskForm {task} {isAdmin} />
+  <TaskForm {task} />
 </div>
