2208122e176485e8dc96b8a083b1c78f053b442f
Kato Hiroki <k.hiro1818@gmail.com>
2024-03-12 14:08:45 +0000

    ✨ Add links for online judges (#151)

--- src/lib/constants/external-links.ts
+++ src/lib/constants/external-links.ts
@@ -1,7 +1,15 @@
-import { ATCODER_BASE_URL, ATCODER_PROBLEMS_URL, GITHUB_URL } from './urls';
+import {
+  ATCODER_BASE_URL,
+  ATCODER_PROBLEMS_URL,
+  AOJ_PCK_URL,
+  AOJ_ICPC_URL,
+  GITHUB_URL,
+} from './urls';
 
 export const externalLinks = [
   { title: `AtCoder`, path: ATCODER_BASE_URL },
   { title: `AtCoder Problems`, path: ATCODER_PROBLEMS_URL },
+  { title: `AOJ-PCK`, path: AOJ_PCK_URL },
+  { title: `AOJ-ICPC`, path: AOJ_ICPC_URL },
   { title: `GitHub`, path: GITHUB_URL },
 ];
--- src/lib/constants/urls.ts
+++ src/lib/constants/urls.ts
@@ -4,4 +4,8 @@ export const ATCODER_BASE_CONTEST_URL: string = `${ATCODER_BASE_URL}/contests`;
 
 export const ATCODER_PROBLEMS_URL: string = 'https://kenkoooo.com/atcoder/#/';
 
+export const AOJ_PCK_URL: string = 'https://pro-ktmr.github.io/aoj-pck/';
+
+export const AOJ_ICPC_URL: string = 'http://aoj-icpc.ichyo.jp/';
+
 export const GITHUB_URL: string = 'https://github.com/AtCoder-NoviSteps/AtCoderNoviSteps';
