ddd77a07e7a21f484e58ccc1a83ac31c4c375294
Kato Hiroki <k.hiro1818@gmail.com>
2024-03-13 12:55:00 +0000

    ✨ Add links for online judges (#540)

--- src/lib/constants/urls.ts
+++ src/lib/constants/urls.ts
@@ -4,6 +4,8 @@ export const ATCODER_BASE_CONTEST_URL: string = `${ATCODER_BASE_URL}/contests`;
 
 export const ATCODER_PROBLEMS_URL: string = 'https://kenkoooo.com/atcoder/#/';
 
+export const AOJ_ATCODER_JOI_URL: string = 'https://joi.goodbaton.com/';
+
 export const AOJ_PCK_URL: string = 'https://pro-ktmr.github.io/aoj-pck/';
 
 export const AOJ_ICPC_URL: string = 'http://aoj-icpc.ichyo.jp/';
