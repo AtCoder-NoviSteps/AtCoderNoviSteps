d989665b57933036db56e5eb2ed6f1f9ed14fee5
Kato Hiroki <k.hiro1818@gmail.com>
2024-03-16 14:14:11 +0000

    ✨ Show message before deleting account (#544)

--- src/lib/components/FormWrapper.svelte
+++ src/lib/components/FormWrapper.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import { enhance } from '$app/forms';
+
   export let method: string = 'POST';
   export let action: string;
   export let marginTop: string = 'mt-6';
   export let spaceYAxis: string = 'space-y-6';
 </script>
 
-<form {method} {action} class={`w-full max-w-md ${marginTop} ${spaceYAxis}`}>
+<form {method} {action} class={`w-full max-w-md ${marginTop} ${spaceYAxis}`} use:enhance>
   <slot />
 </form>
--- src/lib/components/UserAccountDeletionForm.svelte
+++ src/lib/components/UserAccountDeletionForm.svelte
@@ -1,43 +1,53 @@
 <script lang="ts">
-  import { Input, Button, Alert, Checkbox, Li, List } from 'flowbite-svelte';
+  import { Input, Button, Alert, Checkbox, Modal } from 'flowbite-svelte';
   // @ts-ignore
   import InfoCircleSolid from 'flowbite-svelte-icons/InfoCircleSolid.svelte';
 
   import ContainerWrapper from '$lib/components/ContainerWrapper.svelte';
   import FormWrapper from '$lib/components/FormWrapper.svelte';
   import ReadOnlyLabel from '$lib/components/ReadOnlyLabel.svelte';
+  import WarningMessageOnDeletingAccount from '$lib/components/WarningMessageOnDeletingAccount.svelte';
 
   export let username: string;
 
   let showDeleteButton = false;
+  let defaultModal = false;
 
   const toggleDeleteButton = () => {
     showDeleteButton = !showDeleteButton;
   };
+
+  const openModal = () => {
+    defaultModal = true;
+  };
 </script>
 
 <ContainerWrapper>
-  <FormWrapper action="?/delete">
+  <FormWrapper action="">
     <Alert color="red" class="!items-start">
       <InfoCircleSolid slot="icon" class="w-6 h-6" />
       <span class="font-medium text-lg">警告</span>
-      <p class="font-medium">以下の内容が削除されます</p>
-
-      <List class="mt-1.5 ms-4 list-disc list-inside">
-        <Li>アカウント</Li>
-        <Li>登録した回答状況</Li>
-        <Li>AtCoder IDとの連携</Li>
-      </List>
+      <WarningMessageOnDeletingAccount />
     </Alert>
-
-    <!-- hiddenでusernameを持つのは共通 -->
-    <Input size="sm" type="hidden" name="username" bind:value={username} />
 
     <ReadOnlyLabel labelName="ユーザ名" inputValue={username} />
     <Checkbox on:click={toggleDeleteButton}>同意する</Checkbox>
 
     {#if showDeleteButton}
-      <Button readonly type="submit" class="w-full">アカウントを削除</Button>
+      <Button on:click={openModal} class="w-full">アカウントを削除</Button>
+
+      <Modal size="xs" bind:open={defaultModal} outsideclose>
+        <p class="font-medium text-lg text-center">{username}さん、本当に削除しますか?</p>
+        <WarningMessageOnDeletingAccount />
+
+        <FormWrapper action="?/delete">
+          <!-- hiddenでusernameを持つのは共通 -->
+          <Input size="sm" type="hidden" name="username" bind:value={username} />
+          <Button readonly type="submit" class="w-full">
+            読んで理解したので、アカウントを削除
+          </Button>
+        </FormWrapper>
+      </Modal>
     {/if}
   </FormWrapper>
 </ContainerWrapper>
