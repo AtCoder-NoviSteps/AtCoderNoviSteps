c40495a7798a36b82f7865ee3d7401c7d2d0314f
Kato Hiroki <k.hiro1818@gmail.com>
2024-03-23 12:23:01 +0000

    :books: Update descriptions (#558)

--- src/lib/constants/product-info.ts
+++ src/lib/constants/product-info.ts
@@ -6,4 +6,5 @@ export const PRODUCT_TEAM_URL: string = 'https://github.com/orgs/AtCoder-NoviSte
 
 export const PRODUCT_CATCH_PHRASE: string = '解けた喜び、伸びる楽しさ';
 
-export const PRODUCT_DESCRIPTION: string = '';
+export const PRODUCT_DESCRIPTION: string =
+  '【非公式】 AtCoder 上の問題について、取組み状況を記録していくサイトです。各問題が細かく難易度付けされており、必要な知識を段階的に習得できます。';
