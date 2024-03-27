51bd44526d18376fbe3b5bffac704d616a9e4c73
Kato Hiroki <k.hiro1818@gmail.com>
2024-03-27 14:01:06 +0000

    :books: Fix descriptions (#584)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,7 +31,7 @@
     上の問題について、取組み状況を記録していくサイトです。 各問題が細かく難易度付けされており、必要な知識を段階的に習得できます。
   </P>
   <div class="flex flex-wrap md:justify-center md:items-center">
-    <Button href="/problems" class="w-screen md:w-1/3 m-2">問題を解く</Button>
+    <Button href="/problems" class="w-screen md:w-1/3 m-2">問題一覧へ</Button>
     <Button color="alternative" href="/about" class="w-screen md:w-1/3 m-2">
       使い方を見る
       <ArrowRightOutline class="w-3.5 h-3.5 ml-2" />
@@ -60,9 +60,9 @@
   </div>
 
   <Heading tag="h2" class="mt-6 mb-3" customSize="text-xl font-medium  md:text-2xl lg:text-3xl">
-    ちょっと難しい問題に挑戦
+    一歩先の問題に挑戦
   </Heading>
-  <p>問題が17段階で難易度付けされており、少しずつ新しい知識を身につけられます</p>
+  <p>17段階で難易度付けされており、自分の実力に合った問題が探せます</p>
 
   <div class="flex flex-wrap justify-center items-center">
     <Img class="mt-4" src="../../grade_11Q_6Q.png" alt="grade from 11Q to 6Q" />
--- tests/signin.test.ts
+++ tests/signin.test.ts
@@ -3,7 +3,7 @@ import { PRODUCT_CATCH_PHRASE } from '../src/lib/constants/product-info.ts';
 
 test('login logout', async ({ page }) => {
   await page.goto('/');
-  await page.getByRole('button', { name: '問題を解く' }).click();
+  await page.getByRole('button', { name: '問題一覧へ' }).click();
   await page.locator('input[name="username"]').fill('guest');
   //await page.locator('input[name="username"]').fill('guest_user');//TODO: global.setup.tsで作ったユーザでログインできるようにしたい。
   await page.locator('input[name="password"]').fill('Ch0kuda1');
