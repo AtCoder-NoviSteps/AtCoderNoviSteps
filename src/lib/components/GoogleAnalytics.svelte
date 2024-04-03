a2a1a296bad84bc7f6dddd00eabe34728c08c103
Kato Hiroki <k.hiro1818@gmail.com>
2024-04-03 09:27:30 +0000

    🔧 Use Google Analytics (#623)

---
+++ src/lib/components/GoogleAnalytics.svelte
@@ -0,0 +1,18 @@
+<!-- See: https://joyofcode.xyz/sveltekit-google-analytics -->
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-LFHMP1606H"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', 'G-LFHMP1606H');
+  </script>
+</svelte:head>
