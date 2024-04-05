6fb3dd601e9f69b7b859197f6440a0cfc9baafce
Kato Hiroki <k.hiro1818@gmail.com>
2024-04-05 13:50:35 +0000

    ♻️ Remove old comments (#631)

--- src/lib/components/ThermometerProgressBar.svelte
+++ src/lib/components/ThermometerProgressBar.svelte
@@ -15,7 +15,6 @@
 <!-- https://flowbite.com/docs/components/progress/ -->
 <!-- https://blog.canopas.com/designing-stunning-progress-bars-made-easy-with-tailwind-css-ae620ba7b4be -->
 <!-- https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/css/progressbars -->
-<!-- <div class="w-7/12 md:w-8/12 lg:w-9/12 rounded-full border border-gray-200 p-1"> -->
 <div class="{width} rounded-full border border-gray-200 p-1">
   <div class="rounded-full h-6">
     <div class="overflow-hidden h-6 flex rounded-full bg-white">
