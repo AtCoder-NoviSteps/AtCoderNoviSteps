0084839853323e7db8e8ba0a32b837504473a843
Kato Hiroki <k.hiro1818@gmail.com>
2024-04-22 13:21:46 +0000

    ✨ Add error handlings (#697)

--- src/routes/workbooks/create/+page.server.ts
+++ src/routes/workbooks/create/+page.server.ts
@@ -9,7 +9,13 @@ import type { WorkBook } from '$lib/types/workbook';
 import { BAD_REQUEST, TEMPORARY_REDIRECT } from '$lib/constants/http-response-status-codes';
 
 export const load = async ({ locals }) => {
-  // TODO: ログインしていない場合は、ログイン画面へ遷移させる
+  // ログインしていない場合は、ログイン画面へ遷移させる
+  const session = await locals.auth.validate();
+
+  if (!session) {
+    throw redirect(TEMPORARY_REDIRECT, '/login');
+  }
+
   const user = locals.user;
   const isAdmin = user.role === Roles.ADMIN;
   const form = await superValidate(null, zod(workBookSchema));
@@ -23,12 +29,19 @@ export const actions = {
     const form = await superValidate(request, zod(workBookSchema));
 
     if (!form.valid) {
-      return fail(BAD_REQUEST, { form });
+      return fail(BAD_REQUEST, { form: { ...form, message: 'TODO: エラーメッセージを記述' } });
     }
 
-    // TODO: try-catchを付ける
     const workBook: WorkBook = form.data;
-    await crud.createWorkBook(workBook);
+
+    try {
+      await crud.createWorkBook(workBook);
+    } catch (error) {
+      // TODO: クライアント側のエラー
+      return fail(BAD_REQUEST, { form: { ...form, message: 'TODO: エラーメッセージを記述' } });
+
+      // TODO: サーバー側のエラー
+    }
 
     // TODO: リダイレクトのときもメッセージを表示することはできるか調べる
     throw redirect(TEMPORARY_REDIRECT, '/workbooks');
