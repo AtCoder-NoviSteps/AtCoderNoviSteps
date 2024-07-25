8a3064d2ddf9cb534b9c750ae4085f139e689cc0
Kato Hiroki <k.hiro1818@gmail.com>
2024-07-25 22:19:10 +0000

    🚨 Add tests (#1016)

---
+++ src/test/lib/stores/active_workbook_tab.test.ts
@@ -0,0 +1,211 @@
+import { get } from 'svelte/store';
+import { expect, test } from 'vitest';
+
+import { activeWorkbookTabStore } from '$lib/stores/active_workbook_tab';
+import { WorkBookType } from '$lib/types/workbook';
+
+type InitialState = {
+  workBookType: WorkBookType;
+};
+
+type InitialStates = InitialState[];
+
+type UpdateOnce = InitialState;
+
+type UpdateOnces = UpdateOnce[];
+
+type UpdateTwice = {
+  firstTimeUpdated: WorkBookType;
+  secondTimeUpdated: WorkBookType;
+};
+
+describe('Active workbook tab store', () => {
+  describe('initial values is true only for TEXTBOOK', () => {
+    test('getActiveWorkbookTab(workBookType: TEXTBOOK)', () => {
+      expect(get(activeWorkbookTabStore).get(WorkBookType.TEXTBOOK)).toBeTruthy();
+    });
+  });
+
+  describe('initial value is false except for TEXTBOOK', () => {
+    const testCases = [
+      { workBookType: WorkBookType.SOLUTION },
+      { workBookType: WorkBookType.GENRE },
+      { workBookType: WorkBookType.OTHERS },
+      { workBookType: WorkBookType.CREATED_BY_USER },
+    ];
+    runTests('getActiveWorkbookTab', testCases, ({ workBookType }: InitialState) => {
+      expect(get(activeWorkbookTabStore).get(workBookType)).toBeFalsy();
+    });
+
+    function runTests(
+      testName: string,
+      testCases: InitialStates,
+      testFunction: (testCase: InitialState) => void,
+    ) {
+      test.each(testCases)(`${testName}(workBookType: $workBookType)`, testFunction);
+    }
+  });
+
+  describe('update active workbook tab once', () => {
+    const testCases = [
+      { workBookType: WorkBookType.TEXTBOOK },
+      { workBookType: WorkBookType.GENRE },
+      { workBookType: WorkBookType.OTHERS },
+      { workBookType: WorkBookType.CREATED_BY_USER },
+    ];
+    runTests('updateActiveWorkBookTabOnce', testCases, ({ workBookType }: UpdateOnce) => {
+      activeWorkbookTabStore.setActiveWorkbookTab(workBookType);
+
+      const workBookTabs = get(activeWorkbookTabStore);
+      workBookTabs.forEach((isOpen, activeWorkBookType) => {
+        if (activeWorkBookType === workBookType) {
+          expect(isOpen).toBeTruthy();
+        } else {
+          expect(isOpen).toBeFalsy();
+        }
+      });
+    });
+
+    function runTests(
+      testName: string,
+      testCases: UpdateOnces,
+      testFunction: (testCase: UpdateOnce) => void,
+    ) {
+      test.each(testCases)(`${testName}(workBookType: $workBookType)`, testFunction);
+    }
+  });
+
+  describe('update active workbook tab twice', () => {
+    const testCases = [
+      {
+        firstTimeUpdated: WorkBookType.TEXTBOOK,
+        secondTimeUpdated: WorkBookType.TEXTBOOK,
+      },
+      {
+        firstTimeUpdated: WorkBookType.TEXTBOOK,
+        secondTimeUpdated: WorkBookType.SOLUTION,
+      },
+      {
+        firstTimeUpdated: WorkBookType.TEXTBOOK,
+        secondTimeUpdated: WorkBookType.GENRE,
+      },
+      {
+        firstTimeUpdated: WorkBookType.TEXTBOOK,
+        secondTimeUpdated: WorkBookType.OTHERS,
+      },
+      {
+        firstTimeUpdated: WorkBookType.TEXTBOOK,
+        secondTimeUpdated: WorkBookType.CREATED_BY_USER,
+      },
+      {
+        firstTimeUpdated: WorkBookType.SOLUTION,
+        secondTimeUpdated: WorkBookType.TEXTBOOK,
+      },
+      {
+        firstTimeUpdated: WorkBookType.SOLUTION,
+        secondTimeUpdated: WorkBookType.SOLUTION,
+      },
+      {
+        firstTimeUpdated: WorkBookType.SOLUTION,
+        secondTimeUpdated: WorkBookType.GENRE,
+      },
+      {
+        firstTimeUpdated: WorkBookType.SOLUTION,
+        secondTimeUpdated: WorkBookType.OTHERS,
+      },
+      {
+        firstTimeUpdated: WorkBookType.SOLUTION,
+        secondTimeUpdated: WorkBookType.CREATED_BY_USER,
+      },
+      {
+        firstTimeUpdated: WorkBookType.GENRE,
+        secondTimeUpdated: WorkBookType.TEXTBOOK,
+      },
+      {
+        firstTimeUpdated: WorkBookType.GENRE,
+        secondTimeUpdated: WorkBookType.SOLUTION,
+      },
+      {
+        firstTimeUpdated: WorkBookType.GENRE,
+        secondTimeUpdated: WorkBookType.GENRE,
+      },
+      {
+        firstTimeUpdated: WorkBookType.GENRE,
+        secondTimeUpdated: WorkBookType.OTHERS,
+      },
+      {
+        firstTimeUpdated: WorkBookType.GENRE,
+        secondTimeUpdated: WorkBookType.CREATED_BY_USER,
+      },
+      {
+        firstTimeUpdated: WorkBookType.OTHERS,
+        secondTimeUpdated: WorkBookType.TEXTBOOK,
+      },
+      {
+        firstTimeUpdated: WorkBookType.OTHERS,
+        secondTimeUpdated: WorkBookType.SOLUTION,
+      },
+      {
+        firstTimeUpdated: WorkBookType.OTHERS,
+        secondTimeUpdated: WorkBookType.GENRE,
+      },
+      {
+        firstTimeUpdated: WorkBookType.OTHERS,
+        secondTimeUpdated: WorkBookType.OTHERS,
+      },
+      {
+        firstTimeUpdated: WorkBookType.OTHERS,
+        secondTimeUpdated: WorkBookType.CREATED_BY_USER,
+      },
+      {
+        firstTimeUpdated: WorkBookType.CREATED_BY_USER,
+        secondTimeUpdated: WorkBookType.TEXTBOOK,
+      },
+      {
+        firstTimeUpdated: WorkBookType.CREATED_BY_USER,
+        secondTimeUpdated: WorkBookType.SOLUTION,
+      },
+      {
+        firstTimeUpdated: WorkBookType.CREATED_BY_USER,
+        secondTimeUpdated: WorkBookType.GENRE,
+      },
+      {
+        firstTimeUpdated: WorkBookType.CREATED_BY_USER,
+        secondTimeUpdated: WorkBookType.OTHERS,
+      },
+      {
+        firstTimeUpdated: WorkBookType.CREATED_BY_USER,
+        secondTimeUpdated: WorkBookType.CREATED_BY_USER,
+      },
+    ];
+
+    runTests(
+      'updateTaskGradeTwice',
+      testCases,
+      ({ firstTimeUpdated, secondTimeUpdated }: UpdateTwice) => {
+        activeWorkbookTabStore.setActiveWorkbookTab(firstTimeUpdated);
+        activeWorkbookTabStore.setActiveWorkbookTab(secondTimeUpdated);
+
+        const workBookTabs = get(activeWorkbookTabStore);
+        workBookTabs.forEach((isOpen, activeWorkBookType) => {
+          if (activeWorkBookType === secondTimeUpdated) {
+            expect(isOpen).toBeTruthy();
+          } else {
+            expect(isOpen).toBeFalsy();
+          }
+        });
+      },
+    );
+
+    function runTests(
+      testName: string,
+      testCases: UpdateTwice[],
+      testFunction: (testCase: UpdateTwice) => void,
+    ) {
+      test.each(testCases)(
+        `${testName}(firstTimeUpdated: $firstTimeUpdated, secondTimeUpdated: $secondTimeUpdated)`,
+        testFunction,
+      );
+    }
+  });
+});
