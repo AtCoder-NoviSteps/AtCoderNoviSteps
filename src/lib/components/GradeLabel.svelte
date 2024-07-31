888a60996077f29a05ebbfe6b1898b6ecab9899b
Kato Hiroki <k.hiro1818@gmail.com>
2024-07-31 09:47:49 +0000

    :art: Fix grade icon (#1027)

--- src/lib/components/GradeLabel.svelte
+++ src/lib/components/GradeLabel.svelte
@@ -8,10 +8,16 @@
   $: gradeColor = getTaskGradeColor(taskGrade);
 </script>
 
-<div class="p-1 w-10 text-center rounded-lg {toWhiteTextIfNeeds(grade)} {gradeColor}">
-  {#if taskGrade !== TaskGrade.PENDING}
-    {grade}
-  {:else}
-    {'??'}
-  {/if}
+<div class="rounded-lg border-2 border-white">
+  <div
+    class="p-0.5 xs:p-1 w-8 xs:w-10 text-sm xs:text-md text-center rounded-lg {toWhiteTextIfNeeds(
+      grade,
+    )} {gradeColor}"
+  >
+    {#if taskGrade !== TaskGrade.PENDING}
+      {grade}
+    {:else}
+      {'??'}
+    {/if}
+  </div>
 </div>
