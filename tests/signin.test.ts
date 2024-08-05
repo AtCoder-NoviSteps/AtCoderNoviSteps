15b81cf40c439691c4b2f964447dbeaa3f5e9310
Kato Hiroki <k.hiro1818@gmail.com>
2024-08-05 11:18:19 +0000

    ♻️ Refactoring (#1052)

--- tests/signin.test.ts
+++ tests/signin.test.ts
@@ -1,17 +1,20 @@
 import { test, expect } from '@playwright/test';
 import { PRODUCT_CATCH_PHRASE } from '../src/lib/constants/product-info.ts';
 
+// タイムアップの上限を設定
 const UP_TO_ONE_MINUTE = 60 * 1000; // 60 sec * 1000 ms
+// テスト用のサンプルユーザ名とパスワード
 const USERNAME = 'guest';
 const PASSWORD = 'Ch0kuda1';
 
 // TODO: global.setup.tsで利用できるようにする。
 async function login(page, username, password) {
-  // ログインページをクリック
-  await page.goto('/login');
-  await expect(page).toHaveURL('/login', { timeout: UP_TO_ONE_MINUTE });
+  // ログインページをクリック。
+  const loginUrl = '/login';
+  await page.goto(loginUrl);
+  await expect(page).toHaveURL(loginUrl, { timeout: UP_TO_ONE_MINUTE });
 
-  // ユーザ情報を入力してログインボタンを押す
+  // ユーザ情報を入力してログインボタンを押す。
   // TODO: global.setup.tsで作ったユーザでログインできるようにしたい。
   // await page.locator('input[name="username"]').fill('guest_user');
   await page.locator('input[name="username"]').fill(username);
@@ -22,9 +25,9 @@ async function login(page, username, password) {
   await expect(page).toHaveURL('/problems', { timeout: UP_TO_ONE_MINUTE });
 }
 
-async function logout(page) {
-  // ユーザ名をクリックしてドロップダウンを表示し、ログアウトボタン（確認用）を選択
-  await page.getByRole('link', { name: 'guest' }).click();
+async function logout(page, username) {
+  // ユーザ名をクリックしてドロップダウンを表示し、ログアウトボタン（確認用）を選択。
+  await page.getByRole('link', { name: username }).click();
   await page.locator('button[name="logout_helper"]').click();
 
   // 確認用画面（モーダル）のログアウトボタンを押すと、ホーム画面に戻る。
@@ -40,5 +43,5 @@ test('login', async ({ page }) => {
 
 test('logout', async ({ page }) => {
   await login(page, USERNAME, PASSWORD);
-  await logout(page);
+  await logout(page, USERNAME);
 });
