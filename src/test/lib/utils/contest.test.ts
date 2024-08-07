17dc2295bf638ca61324716b72b1dd377e712988
Kato Hiroki <k.hiro1818@gmail.com>
2024-08-07 12:31:33 +0000

    🚨 Add tests for contest type (#1074)

---
+++ src/test/lib/utils/contest.test.ts
@@ -0,0 +1,111 @@
+import { expect, test } from 'vitest';
+
+import { classifyContest } from '$lib/utils/contest';
+import { ContestType } from '$lib/types/contest';
+
+type TestCaseForContestType = {
+  contestId: string;
+  expected: ContestType;
+};
+
+type TestCasesForContestType = TestCaseForContestType[];
+
+describe('Contest', () => {
+  // TODO: Add ARC and AOI.
+  describe('classify contest', () => {
+    test('when contest_id is ABS', () => {
+      expect(classifyContest('abs')).toEqual(ContestType.ABS);
+    });
+
+    describe('when contest_id contains ABC', () => {
+      const testCases = [
+        { contestId: 'abc001', expected: ContestType.ABC },
+        { contestId: 'abc002', expected: ContestType.ABC },
+        { contestId: 'abc099', expected: ContestType.ABC },
+        { contestId: 'abc100', expected: ContestType.ABC },
+        { contestId: 'abc101', expected: ContestType.ABC },
+        { contestId: 'abc200', expected: ContestType.ABC },
+        { contestId: 'abc201', expected: ContestType.ABC },
+        { contestId: 'abc365', expected: ContestType.ABC },
+        { contestId: 'abc999', expected: ContestType.ABC },
+      ];
+
+      runTests('classifyContest', testCases, ({ contestId, expected }: TestCaseForContestType) => {
+        expect(classifyContest(contestId)).toEqual(expected);
+      });
+    });
+
+    test('when contest_id starts with APG4b', () => {
+      expect(classifyContest('APG4b')).toEqual(ContestType.APG4B);
+    });
+
+    test('when contest_id is typical90', () => {
+      expect(classifyContest('typical90')).toEqual(ContestType.TYPICAL90);
+    });
+
+    test('when contest_id is dp (EDPC)', () => {
+      expect(classifyContest('dp')).toEqual(ContestType.EDPC);
+    });
+
+    test('when contest_id is tdpc', () => {
+      expect(classifyContest('tdpc')).toEqual(ContestType.TDPC);
+    });
+
+    describe('when contest_id contains past', () => {
+      const testCases = [
+        { contestId: 'past17-open', expected: ContestType.PAST },
+        { contestId: 'past16-open', expected: ContestType.PAST },
+        { contestId: 'past15-open', expected: ContestType.PAST },
+        { contestId: 'past202303-open', expected: ContestType.PAST }, // PAST 14th
+        { contestId: 'past202212-open', expected: ContestType.PAST }, // PAST 13rd
+        { contestId: 'past202005-open', expected: ContestType.PAST }, // PAST 3rd
+        { contestId: 'past202004-open', expected: ContestType.PAST }, // PAST 2nd
+        { contestId: 'past201912-open', expected: ContestType.PAST }, // PAST 1st
+      ];
+
+      runTests('classifyContest', testCases, ({ contestId, expected }: TestCaseForContestType) => {
+        expect(classifyContest(contestId)).toEqual(expected);
+      });
+    });
+
+    test('when contest_id is practice2 (ACL practice)', () => {
+      expect(classifyContest('practice2')).toEqual(ContestType.ACL_PRACTICE);
+    });
+
+    describe('when contest_id contains joi', () => {
+      const testCases = [
+        { contestId: 'joig2024-open', expected: ContestType.JOI },
+        { contestId: 'joig2023-open', expected: ContestType.JOI },
+        { contestId: 'joig2022-open', expected: ContestType.JOI },
+        { contestId: 'joig2021-open', expected: ContestType.JOI },
+        { contestId: 'joi2024yo1a', expected: ContestType.JOI },
+        { contestId: 'joi2024yo1b', expected: ContestType.JOI },
+        { contestId: 'joi2024yo1c', expected: ContestType.JOI },
+        { contestId: 'joi2023yo1a', expected: ContestType.JOI },
+        { contestId: 'joi2023yo1b', expected: ContestType.JOI },
+        { contestId: 'joi2023yo1c', expected: ContestType.JOI },
+        { contestId: 'joi2018yo', expected: ContestType.JOI },
+      ];
+
+      runTests('classifyContest', testCases, ({ contestId, expected }: TestCaseForContestType) => {
+        expect(classifyContest(contestId)).toEqual(expected);
+      });
+    });
+
+    test('when contest_id is tessoku-book', () => {
+      expect(classifyContest('tessoku-book')).toEqual(ContestType.TESSOKU_BOOK);
+    });
+
+    test('when contest_id is math-and-algorithm', () => {
+      expect(classifyContest('math-and-algorithm')).toEqual(ContestType.MATH_AND_ALGORITHM);
+    });
+
+    function runTests(
+      testName: string,
+      testCases: TestCasesForContestType,
+      testFunction: (testCase: TestCaseForContestType) => void,
+    ) {
+      test.each(testCases)(`${testName}(contestId: $contestId)`, testFunction);
+    }
+  });
+});
