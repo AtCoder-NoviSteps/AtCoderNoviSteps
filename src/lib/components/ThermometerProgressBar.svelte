82d7ecb3ac9dd7ef3d621661b5c6fead1c39d04b
Kato Hiroki <k.hiro1818@gmail.com>
2024-08-07 07:55:36 +0000

    :art: Show breakdown of submission status when hovering over progress bar (#1072)

--- src/lib/components/ThermometerProgressBar.svelte
+++ src/lib/components/ThermometerProgressBar.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import { Tooltip } from 'flowbite-svelte';
+
   import type { WorkBookTaskBase } from '$lib/types/workbook';
   import type { TaskResult, TaskResults } from '$lib/types/task';
-  import type { SubmissionRatios } from '$lib/types/submission';
+  import type { SubmissionRatios, SubmissionCounts } from '$lib/types/submission';
   import { submission_statuses } from '$lib/services/submission_status';
 
   export let workBookTasks: WorkBookTaskBase[] = [];
@@ -9,31 +11,46 @@
   export let width: string = 'w-7/12 md:w-8/12 lg:w-9/12';
 
   let submissionRatios: SubmissionRatios = [];
+  let submissionCounts: SubmissionCounts = [];
+  let progressBarId = `progress-bar-${Math.floor(Math.random() * 10000)}`;
+
+  const filteredStatuses = submission_statuses.filter((status) => status.status_name !== 'ns');
 
   // TODO: ユーザの設定に応じて、ACかどうかの判定を変更できるようにする
-  $: {
-    const filteredStatuses = submission_statuses.filter((status) => status.status_name !== 'ns');
+  $: submissionRatios = filteredStatuses.map((status) => {
+    const name = status.status_name;
+    const ratioPercent = getRatioPercent(taskResults, name);
 
-    submissionRatios = filteredStatuses.map((status) => {
-      const name = status.status_name;
-      const ratioPercent = getRatioPercent(taskResults, name);
+    return {
+      name: name,
+      ratioPercent: ratioPercent,
+      color: status.background_color,
+    };
+  });
 
-      return {
-        name: name,
-        ratioPercent: ratioPercent,
-        color: status.background_color,
-      };
-    });
-  }
+  $: submissionCounts = filteredStatuses.map((status) => {
+    const name = status.status_name;
+    const taskCount = getTaskCount(taskResults, name);
+    const ratioPercent = getRatioPercent(taskResults, name);
+
+    return {
+      name: status.label_name,
+      count: taskCount,
+      ratioPercent: ratioPercent,
+    };
+  });
 
   const getRatioPercent = (taskResults: TaskResults, statusName: string) => {
-    const filteredTaskCount = taskResults.filter(
-      (taskResult: TaskResult) => taskResult.status_name === statusName,
-    ).length;
+    const filteredTaskCount = getTaskCount(taskResults, statusName);
     const allTaskCount = getAllTaskCount();
     const ratioPercent = allTaskCount ? (filteredTaskCount / allTaskCount) * 100 : 0;
 
     return ratioPercent;
+  };
+
+  const getTaskCount = (taskResults: TaskResults, statusName: string) => {
+    return taskResults.filter((taskResult: TaskResult) => taskResult.status_name === statusName)
+      .length;
   };
 
   const getAllTaskCount = () => {
@@ -53,13 +70,25 @@
 <!-- https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/css/progressbars -->
 <div class="{width} rounded-full border border-gray-200 p-1">
   <div class="rounded-full h-6">
-    <div class="overflow-hidden h-6 flex rounded-full bg-white">
-      {#each submissionRatios as submissionRatio}
-        <div
-          style="width: {submissionRatio.ratioPercent}%"
-          class={`${baseAttributes} ${submissionRatio.color}`}
-        ></div>
-      {/each}
-    </div>
+    <span id={progressBarId}>
+      <div class="overflow-hidden h-6 flex rounded-full bg-white">
+        {#each submissionRatios as submissionRatio}
+          <div
+            style="width: {submissionRatio.ratioPercent}%"
+            class={`${baseAttributes} ${submissionRatio.color}`}
+          ></div>
+        {/each}
+      </div>
+    </span>
   </div>
 </div>
+
+<Tooltip type="auto" triggeredBy={`#${progressBarId}`} placement="top-start" class="max-w-[200px]">
+  {#each submissionCounts as submissionCount}
+    <div class="flex">
+      <span class="w-14">{submissionCount.name}</span>
+      <span class="w-2">: </span>
+      <span>{submissionCount.count} ({submissionCount.ratioPercent.toFixed(1)}%)</span>
+    </div>
+  {/each}
+</Tooltip>
