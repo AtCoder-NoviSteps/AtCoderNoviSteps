2478d123aff04debf22f51bd2aae147c69df6b24
Kato Hiroki <k.hiro1818@gmail.com>
2024-08-14 10:52:52 +0000

    :art: Fix style (#1105)

--- src/lib/components/MessageHelperWrapper.svelte
+++ src/lib/components/MessageHelperWrapper.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { Helper } from 'flowbite-svelte';
 
+  export let marginTop = 'mt-2';
   export let message;
 </script>
 
 {#if message}
-  <Helper class="mt-2 dark:text-red-500" color="red">
+  <Helper class="{marginTop} dark:text-red-500" color="red">
     <span class="font-medium">{message}</span>
   </Helper>
 {/if}
