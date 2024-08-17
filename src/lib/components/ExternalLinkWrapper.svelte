41e9908b5168ccccd741c00eb0a0d61d9c121034
Kato Hiroki <k.hiro1818@gmail.com>
2024-08-17 06:19:11 +0000

    :art: Add [↪] to external links (#1139)

--- src/lib/components/ExternalLinkWrapper.svelte
+++ src/lib/components/ExternalLinkWrapper.svelte
@@ -13,3 +13,17 @@
 >
   {description}
 </a>
+
+<!-- See: -->
+<!-- https://github.com/fastapi/fastapi/blob/master/docs/en/docs/css/custom.css -->
+<style>
+  a::after {
+    /* \00A0 is a non-breaking space
+        to make the mark be on the same line as the link
+    */
+    content: '\00A0[↪]';
+    font-family: Roboto, Arial, sans-serif;
+    position: relative;
+    top: -0.1em;
+  }
+</style>
