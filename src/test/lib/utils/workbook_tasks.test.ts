b9605061b48bb4aca8ec86309a689a7361c4ceda
Kato Hiroki <k.hiro1818@gmail.com>
2024-08-26 22:08:04 +0000

    🚨 Add tests for adding task to workbook (#1140)

--- src/test/lib/utils/workbook_tasks.test.ts
+++ src/test/lib/utils/workbook_tasks.test.ts
@@ -5,10 +5,14 @@ import type {
   WorkBookTasksCreate,
   WorkBookTasksEdit,
 } from '$lib/types/workbook';
-import type { Tasks } from '$lib/types/task';
+import type { Task, Tasks } from '$lib/types/task';
 import { TaskGrade } from '$lib/types/task';
 
-import { updateWorkBookTasks, updateWorkBookTaskForTable } from '$lib/utils/workbook_tasks';
+import {
+  updateWorkBookTasks,
+  updateWorkBookTaskForTable,
+  addTaskToWorkBook,
+} from '$lib/utils/workbook_tasks';
 
 // Note: The selectedIndex and selectedTask are arrays so that one or more tasks can be tested with one object.
 type TestCaseForWorkBookTasks = {
@@ -29,6 +33,19 @@ type TestCaseForWorkBookTasksForTable = {
 
 type TestCasesForWorkBookTasksForTable = TestCaseForWorkBookTasksForTable[];
 
+type TestCaseForAddingTasksToWorkBook = {
+  selectedTask: Task;
+  workBookTasks: WorkBookTasksBase;
+  workBookTasksForTable: WorkBookTasksCreate | WorkBookTasksEdit;
+  newWorkBookTaskIndex: number;
+  expected: {
+    updatedWorkBookTasks: WorkBookTasksBase;
+    updatedWorkBookTasksForTable: WorkBookTasksCreate | WorkBookTasksEdit;
+  };
+};
+
+type TestCasesForAddingTasksToWorkBook = TestCaseForAddingTasksToWorkBook[];
+
 // Note: Existing workbook tasks and new selected tasks to be added.
 const baseWorkBookTasks = [
   { taskId: 'abc307_c', priority: 1, comment: '' },
@@ -41,6 +58,14 @@ const baseWorkBookTasksForTable = [
   { taskId: 'abc319_c', priority: 2, comment: '', contestId: 'abc319', title: 'C. False Hope' },
   { taskId: 'abc322_d', priority: 3, comment: '', contestId: 'abc322', title: 'D. Polyomino' },
 ];
+
+const newSelectedTask = {
+  contest_id: 'abc347',
+  task_table_index: 'C',
+  task_id: 'abc347_c',
+  title: 'C. Ideal Holidays',
+  grade: TaskGrade.Q1,
+};
 
 const newSelectedTasks = [
   {
@@ -1004,6 +1029,384 @@ describe('Workbook tasks', () => {
     ) {
       test.each(testCases)(
         `${testName}(workBookTasks: $workBookTasks, selectedIndexes: $selectedIndexes, selectedTasks: $selectedTasks)`,
+        testFunction,
+      );
+    }
+  });
+
+  // Note: It is basically almost the same as the updateWorkBookTasks() and updateWorkBookTaskForTable() method, so only minimal differences are tested.
+  describe('add tasks to workbook tasks', () => {
+    describe('when adding a task to an empty workbook task', () => {
+      const testCases: TestCasesForAddingTasksToWorkBook = [
+        {
+          selectedTask: newSelectedTask,
+          workBookTasks: [],
+          workBookTasksForTable: [],
+          newWorkBookTaskIndex: 0, // 0-indexed
+          expected: {
+            updatedWorkBookTasks: [
+              {
+                taskId: 'abc347_c',
+                priority: 1,
+                comment: '',
+              },
+            ],
+            updatedWorkBookTasksForTable: [
+              {
+                taskId: 'abc347_c',
+                priority: 1,
+                comment: '',
+                contestId: 'abc347',
+                title: 'C. Ideal Holidays',
+              },
+            ],
+          },
+        },
+      ];
+
+      runTests(
+        'addTaskToWorkBook',
+        testCases,
+        ({
+          selectedTask,
+          workBookTasks,
+          workBookTasksForTable,
+          newWorkBookTaskIndex,
+          expected,
+        }: TestCaseForAddingTasksToWorkBook) => {
+          const newWorkBookTasks = addTaskToWorkBook(
+            selectedTask,
+            workBookTasks,
+            workBookTasksForTable,
+            newWorkBookTaskIndex,
+          );
+          expect(newWorkBookTasks).toStrictEqual(expected);
+        },
+      );
+    });
+
+    describe('when adding a task to existing workbook tasks at the top', () => {
+      const testCases: TestCasesForAddingTasksToWorkBook = [
+        {
+          selectedTask: newSelectedTask,
+          workBookTasks: baseWorkBookTasks,
+          workBookTasksForTable: baseWorkBookTasksForTable,
+          newWorkBookTaskIndex: 0, // 0-indexed
+          expected: {
+            updatedWorkBookTasks: [
+              {
+                taskId: 'abc347_c',
+                priority: 1,
+                comment: '',
+              },
+              {
+                taskId: 'abc307_c',
+                priority: 2,
+                comment: '',
+              },
+              {
+                taskId: 'abc319_c',
+                priority: 3,
+                comment: '',
+              },
+              {
+                taskId: 'abc322_d',
+                priority: 4,
+                comment: '',
+              },
+            ],
+            updatedWorkBookTasksForTable: [
+              {
+                taskId: 'abc347_c',
+                priority: 1,
+                comment: '',
+                contestId: 'abc347',
+                title: 'C. Ideal Holidays',
+              },
+              {
+                taskId: 'abc307_c',
+                priority: 2,
+                comment: '',
+                contestId: 'abc307',
+                title: 'C. Ideal Sheet',
+              },
+              {
+                taskId: 'abc319_c',
+                priority: 3,
+                comment: '',
+                contestId: 'abc319',
+                title: 'C. False Hope',
+              },
+              {
+                taskId: 'abc322_d',
+                priority: 4,
+                comment: '',
+                contestId: 'abc322',
+                title: 'D. Polyomino',
+              },
+            ],
+          },
+        },
+      ];
+
+      runTests(
+        'addTaskToWorkBook',
+        testCases,
+        ({
+          selectedTask,
+          workBookTasks,
+          workBookTasksForTable,
+          newWorkBookTaskIndex,
+          expected,
+        }: TestCaseForAddingTasksToWorkBook) => {
+          const newWorkBookTasks = addTaskToWorkBook(
+            selectedTask,
+            workBookTasks,
+            workBookTasksForTable,
+            newWorkBookTaskIndex,
+          );
+          expect(newWorkBookTasks).toStrictEqual(expected);
+        },
+      );
+    });
+
+    describe('when adding a task to existing workbook tasks at the end', () => {
+      const testCases: TestCasesForAddingTasksToWorkBook = [
+        {
+          selectedTask: newSelectedTask,
+          workBookTasks: baseWorkBookTasks,
+          workBookTasksForTable: baseWorkBookTasksForTable,
+          newWorkBookTaskIndex: 3, // 0-indexed
+          expected: {
+            updatedWorkBookTasks: [
+              {
+                taskId: 'abc307_c',
+                priority: 1,
+                comment: '',
+              },
+              {
+                taskId: 'abc319_c',
+                priority: 2,
+                comment: '',
+              },
+              {
+                taskId: 'abc322_d',
+                priority: 3,
+                comment: '',
+              },
+              {
+                taskId: 'abc347_c',
+                priority: 4,
+                comment: '',
+              },
+            ],
+            updatedWorkBookTasksForTable: [
+              {
+                taskId: 'abc307_c',
+                priority: 1,
+                comment: '',
+                contestId: 'abc307',
+                title: 'C. Ideal Sheet',
+              },
+              {
+                taskId: 'abc319_c',
+                priority: 2,
+                comment: '',
+                contestId: 'abc319',
+                title: 'C. False Hope',
+              },
+              {
+                taskId: 'abc322_d',
+                priority: 3,
+                comment: '',
+                contestId: 'abc322',
+                title: 'D. Polyomino',
+              },
+              {
+                taskId: 'abc347_c',
+                priority: 4,
+                comment: '',
+                contestId: 'abc347',
+                title: 'C. Ideal Holidays',
+              },
+            ],
+          },
+        },
+      ];
+
+      runTests(
+        'addTaskToWorkBook',
+        testCases,
+        ({
+          selectedTask,
+          workBookTasks,
+          workBookTasksForTable,
+          newWorkBookTaskIndex,
+          expected,
+        }: TestCaseForAddingTasksToWorkBook) => {
+          const newWorkBookTasks = addTaskToWorkBook(
+            selectedTask,
+            workBookTasks,
+            workBookTasksForTable,
+            newWorkBookTaskIndex,
+          );
+          expect(newWorkBookTasks).toStrictEqual(expected);
+        },
+      );
+    });
+
+    describe('when adding a task to existing workbook tasks at radom', () => {
+      const testCases: TestCasesForAddingTasksToWorkBook = [
+        {
+          selectedTask: newSelectedTask,
+          workBookTasks: baseWorkBookTasks,
+          workBookTasksForTable: baseWorkBookTasksForTable,
+          newWorkBookTaskIndex: 1, // 0-indexed
+          expected: {
+            updatedWorkBookTasks: [
+              {
+                taskId: 'abc307_c',
+                priority: 1,
+                comment: '',
+              },
+              {
+                taskId: 'abc347_c',
+                priority: 2,
+                comment: '',
+              },
+              {
+                taskId: 'abc319_c',
+                priority: 3,
+                comment: '',
+              },
+              {
+                taskId: 'abc322_d',
+                priority: 4,
+                comment: '',
+              },
+            ],
+            updatedWorkBookTasksForTable: [
+              {
+                taskId: 'abc307_c',
+                priority: 1,
+                comment: '',
+                contestId: 'abc307',
+                title: 'C. Ideal Sheet',
+              },
+              {
+                taskId: 'abc347_c',
+                priority: 2,
+                comment: '',
+                contestId: 'abc347',
+                title: 'C. Ideal Holidays',
+              },
+              {
+                taskId: 'abc319_c',
+                priority: 3,
+                comment: '',
+                contestId: 'abc319',
+                title: 'C. False Hope',
+              },
+              {
+                taskId: 'abc322_d',
+                priority: 4,
+                comment: '',
+                contestId: 'abc322',
+                title: 'D. Polyomino',
+              },
+            ],
+          },
+        },
+        {
+          selectedTask: newSelectedTask,
+          workBookTasks: baseWorkBookTasks,
+          workBookTasksForTable: baseWorkBookTasksForTable,
+          newWorkBookTaskIndex: 2, // 0-indexed
+          expected: {
+            updatedWorkBookTasks: [
+              {
+                taskId: 'abc307_c',
+                priority: 1,
+                comment: '',
+              },
+              {
+                taskId: 'abc319_c',
+                priority: 2,
+                comment: '',
+              },
+              {
+                taskId: 'abc347_c',
+                priority: 3,
+                comment: '',
+              },
+              {
+                taskId: 'abc322_d',
+                priority: 4,
+                comment: '',
+              },
+            ],
+            updatedWorkBookTasksForTable: [
+              {
+                taskId: 'abc307_c',
+                priority: 1,
+                comment: '',
+                contestId: 'abc307',
+                title: 'C. Ideal Sheet',
+              },
+              {
+                taskId: 'abc319_c',
+                priority: 2,
+                comment: '',
+                contestId: 'abc319',
+                title: 'C. False Hope',
+              },
+              {
+                taskId: 'abc347_c',
+                priority: 3,
+                comment: '',
+                contestId: 'abc347',
+                title: 'C. Ideal Holidays',
+              },
+              {
+                taskId: 'abc322_d',
+                priority: 4,
+                comment: '',
+                contestId: 'abc322',
+                title: 'D. Polyomino',
+              },
+            ],
+          },
+        },
+      ];
+
+      runTests(
+        'addTaskToWorkBook',
+        testCases,
+        ({
+          selectedTask,
+          workBookTasks,
+          workBookTasksForTable,
+          newWorkBookTaskIndex,
+          expected,
+        }: TestCaseForAddingTasksToWorkBook) => {
+          const newWorkBookTasks = addTaskToWorkBook(
+            selectedTask,
+            workBookTasks,
+            workBookTasksForTable,
+            newWorkBookTaskIndex,
+          );
+          expect(newWorkBookTasks).toStrictEqual(expected);
+        },
+      );
+    });
+
+    function runTests(
+      testName: string,
+      testCases: TestCasesForAddingTasksToWorkBook,
+      testFunction: (testCase: TestCaseForAddingTasksToWorkBook) => void,
+    ) {
+      test.each(testCases)(
+        `${testName}(selectedTask: $selectedTask, workBookTasks: $workBookTasks, workBookTasksForTable: $workBookTasksForTable, newWorkBookTaskIndex: $newWorkBookTaskIndex)`,
         testFunction,
       );
     }
