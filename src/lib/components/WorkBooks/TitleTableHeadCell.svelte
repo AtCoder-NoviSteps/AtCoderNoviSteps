771bf5001401f6545193a3d7f91afab50a070ff7
Kato Hiroki <k.hiro1818@gmail.com>
2024-09-24 11:24:45 +0000

    ♻️ Extract components (#1293)

---
+++ src/lib/components/WorkBooks/TitleTableHeadCell.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { TableHeadCell } from 'flowbite-svelte';
+
+  export let paddingX: string = 'px-1 xs:px-3';
+</script>
+
+<TableHeadCell
+  class="text-left min-w-[240px] max-w-[240px] lg:max-w-[280px] xl:max-w-[360px] 2xl:max-w-[480px] {paddingX}"
+>
+  タイトル
+</TableHeadCell>
