67c5cdf41ce07bea51cd6f38099d42e469f1a57c
Kato Hiroki <k.hiro1818@gmail.com>
2024-10-16 13:12:13 +0000

    ♻️ Remove old comments (#1169)

--- src/lib/clients/common.ts
+++ src/lib/clients/common.ts
@@ -3,7 +3,6 @@ import { delay } from '$lib/utils/time';
 // See:
 // https://developer.mozilla.org/ja/docs/Web/API/Fetch_API/Using_Fetch
 export async function fetchAPI<T>(url: string, error_messages: string): Promise<T[]> {
-  // export async function fetchAPI(url: string, error_messages: string) {
   try {
     // 外部APIへの過剰なアクセスを防ぐため、1秒待機
     await delay(1000);
@@ -21,7 +20,6 @@ export async function fetchAPI<T>(url: string, error_messages: string): Promise<
 
     return response.json();
   } catch (error) {
-    // TODO: Handle errors.
     console.error(error_messages, error);
     throw error;
   }
