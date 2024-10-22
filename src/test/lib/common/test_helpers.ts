21e7b1c380796404b9b329710ca7223fb3b835b0
Kato Hiroki <k.hiro1818@gmail.com>
2024-10-22 12:56:55 +0000

    ♻️  Add docs (#1346)

--- src/test/lib/common/test_helpers.ts
+++ src/test/lib/common/test_helpers.ts
@@ -1,5 +1,12 @@
 import { test } from 'vitest';
 
+/**
+ * Runs multiple test cases for a given test name using Vitest.
+ * @template T The type of the test cases
+ * @param {string} testName The base name for the tests
+ * @param {T[]} testCases An array of test cases
+ * @param {(testCase: T) => void} testFunction The function to execute for each test case
+ */
 export function runTests<T>(testName: string, testCases: T[], testFunction: (testCase: T) => void) {
   test.each(testCases)(`${testName} - %o`, testFunction);
 }
