684400087b85096593fbf42a3c00be670bacb326
Kato Hiroki <k.hiro1818@gmail.com>
2024-10-23 09:16:58 +0000

    🚨 Add options and tests (#1346)

--- src/lib/utils/html.ts
+++ src/lib/utils/html.ts
@@ -1,5 +1,40 @@
-import xss from 'xss';
+import { type IFilterXSSOptions, FilterXSS } from 'xss';
 
-export function sanitizeHTML(html: string): string {
-  return xss(html);
+interface SanitizeOptions {
+  allowedTags?: string[];
+  allowedAttributes?: { [key: string]: string[] };
+}
+
+export function sanitizeHTML(html: string, options?: SanitizeOptions): string {
+  const defaultOptions: IFilterXSSOptions = {
+    whiteList: {
+      a: ['href'],
+      b: [],
+      em: [],
+      i: [],
+      strong: [],
+    },
+  };
+
+  if (options?.allowedTags) {
+    defaultOptions.whiteList = options.allowedTags.reduce(
+      (array, tag) => {
+        array[tag] = (defaultOptions.whiteList && defaultOptions.whiteList[tag]) || [];
+        return array;
+      },
+      {} as { [key: string]: string[] },
+    );
+  }
+
+  if (options?.allowedAttributes) {
+    Object.keys(options.allowedAttributes).forEach((tag) => {
+      if (defaultOptions.whiteList && options.allowedAttributes) {
+        defaultOptions.whiteList[tag] = options.allowedAttributes[tag];
+      }
+    });
+  }
+
+  const customXSS = new FilterXSS(defaultOptions);
+
+  return customXSS.process(html);
 }
