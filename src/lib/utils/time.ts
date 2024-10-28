5cb5f6b94937268cd405fa9947a5bffb839f9714
Kato Hiroki <k.hiro1818@gmail.com>
2024-10-28 12:16:13 +0000

    ♻️ Remove unused utils (#1169)

--- src/lib/utils/time.ts
+++ src/lib/utils/time.ts
@@ -7,14 +7,3 @@ export function delay(milliseconds: number) {
 
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
-
-// See:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#get_the_number_of_seconds_since_the_ecmascript_epoch
-export const dateToUnixSecond = (date: number | Date | null) => {
-  if (date === null) {
-    return 0;
-  }
-
-  // @ts-ignore
-  return Math.floor(date / 1000);
-};
