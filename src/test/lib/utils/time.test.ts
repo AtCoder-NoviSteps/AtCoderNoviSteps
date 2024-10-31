c0eb4029300c17fd01fb27ac412c9ac7df8cb426
Kato Hiroki <k.hiro1818@gmail.com>
2024-10-31 09:40:04 +0000

    🐎 Use performance.now() (#1169)

--- src/test/lib/utils/time.test.ts
+++ src/test/lib/utils/time.test.ts
@@ -3,10 +3,10 @@ import { delay } from '$lib/utils/time';
 
 describe('Delay', () => {
   it('expected to be resolved after the specified delay', async () => {
-    const start = Date.now();
+    const start = performance.now();
     const milliseconds = 100;
     await delay(milliseconds);
-    const end = Date.now();
+    const end = performance.now();
 
     expect(end - start).toBeGreaterThanOrEqual(milliseconds);
   });
@@ -16,10 +16,10 @@ describe('Delay', () => {
   });
 
   it('expected to be resolved immediately if the delay duration is zero', async () => {
-    const start = Date.now();
+    const start = performance.now();
     await delay(0);
-    const end = Date.now();
+    const end = performance.now();
 
-    expect(end - start).toBeLessThan(10); // Allowing a small margin for execution time.
+    expect(end - start).toBeLessThan(50); // Increased threshold for CI environment reliability.
   });
 });
