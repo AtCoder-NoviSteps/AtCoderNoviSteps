056e111a33a1ec880ecf0d39939f0c210ef72248
Kato Hiroki <k.hiro1818@gmail.com>
2024-11-02 08:47:25 +0000

    :books: Add docs (#1169)

--- src/lib/clients/atcoder_problems.ts
+++ src/lib/clients/atcoder_problems.ts
@@ -4,6 +4,12 @@ import type { ContestsForImport } from '$lib/types/contest';
 import type { TasksForImport } from '$lib/types/task';
 
 export class AtCoderProblemsApiClient extends ContestSiteApiClient {
+  /**
+   * Fetches the list of contests from the AtCoder Problems API.
+   *
+   * @returns {Promise<ContestsForImport>} A promise that resolves to the list of contests.
+   * @throws Will throw an error if the fetch operation fails or if the response is invalid.
+   */
   async getContests(): Promise<ContestsForImport> {
     const contests = await this.fetchApiWithConfig<ContestsForImport>({
       baseApiUrl: ATCODER_PROBLEMS_API_BASE_URL,
@@ -17,6 +23,12 @@ export class AtCoderProblemsApiClient extends ContestSiteApiClient {
     return contests;
   }
 
+  /**
+   * Fetches tasks from the AtCoder Problems API.
+   *
+   * @returns {Promise<TasksForImport>} A promise that resolves to an array of tasks for import.
+   * @throws Will throw an error if the fetch operation fails or if the response validation fails.
+   */
   async getTasks(): Promise<TasksForImport> {
     const tasks = await this.fetchApiWithConfig<TasksForImport>({
       baseApiUrl: ATCODER_PROBLEMS_API_BASE_URL,
