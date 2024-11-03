09c2b01e4438c302284e2b28672871aa235e0fee
Kato Hiroki <k.hiro1818@gmail.com>
2024-11-03 02:20:19 +0000

    :art: Add loading component (#1169)

---
+++ src/lib/components/SpinnerWrapper.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Spinner from 'flowbite-svelte/Spinner.svelte';
+
+  export let size = 12;
+</script>
+
+<div class="container mx-auto text-left">
+  <Spinner class="m-8" {size} />
+</div>
