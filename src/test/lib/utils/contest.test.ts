41bba20228812ade73d62f677cedf1699ac28722
Kato Hiroki <k.hiro1818@gmail.com>
2024-11-29 14:03:07 +0000

    🚨 Add and update tests for university contest label (#1526)

--- src/test/lib/utils/contest.test.ts
+++ src/test/lib/utils/contest.test.ts
@@ -548,22 +548,19 @@ describe('Contest', () => {
   });
 
   describe('get AtCoder university contest label', () => {
+    describe('expected to return correct label for valid format', () => {
+      test.each([
+        ['utpc2019', 'UTPC 2019'],
+        ['ttpc2022', 'TTPC 2022'],
+      ])('when %s is given', (input, expected) => {
+        expect(getAtCoderUniversityContestLabel(input)).toBe(expected);
+      });
+    });
+
     describe('expected to be thrown an error if an invalid format is given', () => {
-      test('when utpc is given', () => {
-        expect(() => getAtCoderUniversityContestLabel('utpc24')).toThrow(
-          'Invalid university contest ID format: utpc24',
-        );
-      });
-
-      test('when ttpc is given', () => {
-        expect(() => getAtCoderUniversityContestLabel('ttpc')).toThrow(
-          'Invalid university contest ID format: ttpc',
-        );
-      });
-
-      test('when tupc is given', () => {
-        expect(() => getAtCoderUniversityContestLabel('tupc')).toThrow(
-          'Invalid university contest ID format: tupc',
+      test.each(['utpc24', 'ttpc', 'tupc'])('when %s is given', (input) => {
+        expect(() => getAtCoderUniversityContestLabel(input)).toThrow(
+          `Invalid university contest ID format: ${input}`,
         );
       });
     });
