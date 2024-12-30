4416a23050312e99f2b911e50bd42dcaece83417
Kato Hiroki <k.hiro1818@gmail.com>
2024-12-30 06:48:16 +0000

    :bug: Fix conflict (#1611)

--- src/lib/components/TaskTables/TaskTable.svelte
+++ src/lib/components/TaskTables/TaskTable.svelte
@@ -16,10 +16,9 @@
   import { getTaskTableHeaderName } from '$lib/utils/task';
   export let taskResults: TaskResults;
 
-  let selectedContestType: ContestType;
-  let selectedTaskResults: TaskResults | null = null;
-  let contestNames: Array<string> | null = null;
-  let headerNames: Array<string> | null = null;
+  let selectedTaskResults: TaskResults;
+  let contestNames: Array<string>;
+  let headerNames: Array<string>;
 
   $: selectedTaskResults = filterTaskResultsByContestType(fromABC212_Onwards);
   $: contestNames = getContestNames(selectedTaskResults);
@@ -52,41 +51,36 @@
     classifyContest(taskResult.contest_id) === ContestType.ABC && taskResult.contest_id >= 'abc212';
 </script>
 
-<!-- TODO: ボタンの並び順を決める -->
+<!-- TODO: コンテスト種別のボタンの並び順を決める -->
 <!-- See: -->
 <!-- https://flowbite-svelte.com/docs/components/button-group -->
 <ButtonGroup class="m-4 contents-center">
   <Button on:click={() => filterTaskResultsByContestType(fromABC212_Onwards)}>ABC212〜</Button>
 </ButtonGroup>
 
-<!-- TODO: 該当する問題をテーブル形式で表示する -->
+<!-- TODO: コンテスト種別に応じて変更できるようにする -->
 <h2 class="dark:text-gray-100">AtCoder Beginners Contest</h2>
 
-{#if selectedTaskResults !== null && selectedTaskResults.length}
+{#if selectedTaskResults.length}
   <p>問題数: {selectedTaskResults.length}</p>
 {/if}
 
-<!-- TODO: ABC212〜、〜ABC211で分離 -->
-<!-- FIXME: nullの状態のときは表示しないようにする -->
 <Table shadow hoverable={true}>
   <TableHead>
     <TableHeadCell>Contest</TableHeadCell>
-    {#if headerNames !== null && headerNames.length}
+    {#if headerNames.length}
       {#each headerNames as headerName}
         <TableHeadCell>{headerName}</TableHeadCell>
       {/each}
     {/if}
   </TableHead>
   <TableBody tableBodyClass="divede-y">
-    {#if contestNames !== null && contestNames.length}
-      <!-- HACK: for ABC only. -->
-      {#if selectedContestType === ContestType.ABC}
-        {#each contestNames as contestName}
-          <TableBodyRow>
-            <TableBodyCell>{getContestNameLabel(contestName)}</TableBodyCell>
-          </TableBodyRow>
-        {/each}
-      {/if}
+    {#if contestNames.length}
+      {#each contestNames as contestName}
+        <TableBodyRow>
+          <TableBodyCell>{getContestNameLabel(contestName)}</TableBodyCell>
+        </TableBodyRow>
+      {/each}
     {/if}
   </TableBody>
 </Table>
