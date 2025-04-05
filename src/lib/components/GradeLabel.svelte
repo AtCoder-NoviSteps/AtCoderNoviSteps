d06613aad6fb7bacc2c06630a61c2696f0242cd2
Kato Hiroki <k.hiro1818@gmail.com>
2025-04-05 08:48:27 +0000

    :art: Improve grade colors (#1901)

--- src/lib/components/GradeLabel.svelte
+++ src/lib/components/GradeLabel.svelte
@@ -25,13 +25,23 @@
 
   let grade = $derived(getTaskGradeLabel(taskGrade));
   let gradeColor = $derived(getTaskGradeColor(taskGrade));
+
+  function addGradientIfNeeds(taskGrade: TaskGrade | string) {
+    if (taskGrade === TaskGrade.D6) {
+      const bronze =
+        'bg-gradient-to-br from-atcoder-D6 via-stone-600 to-amber-600 shadow-inner shadow shadow-amber-900/80 ring-2 ring-amber-300/50 text-amber-50 font-bold drop-shadow relative overflow-hidden';
+      return bronze;
+    }
+    return '';
+  }
+
+  let gradient = $derived(addGradientIfNeeds(taskGrade));
 </script>
 
-<div class="rounded-lg border-2 {toChangeBorderColorIfNeeds(grade)}">
+<div class="rounded-lg border-2 {toChangeBorderColorIfNeeds(grade)} {gradient ? 'shadow-md' : ''}">
   <div
-    class="p-{defaultPadding} w-{reducedWidth} xs:w-{defaultWidth} text-sm xs:text-{defaultTextSize} text-center rounded-md {toChangeTextColorIfNeeds(
-      grade,
-    )} {gradeColor}"
+    class="p-{defaultPadding} w-{reducedWidth} xs:w-{defaultWidth} text-sm xs:text-{defaultTextSize} text-center rounded-md
+    {gradient || toChangeTextColorIfNeeds(grade) + ' ' + gradeColor}"
   >
     {#if taskGrade !== TaskGrade.PENDING}
       {grade}
