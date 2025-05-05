8e223e66bd7a51b1d45ac39774a17c2195fb8573
Kato Hiroki <k.hiro1818@gmail.com>
2025-05-05 12:58:36 +0000

    ✨ Add clients for http (#1995)

--- src/lib/clients/http_client.ts
+++ src/lib/clients/http_client.ts
@@ -3,6 +3,63 @@ import type { TasksForImport } from '$lib/types/task';
 import { delay } from '$lib/utils/time';
 
 /**
+ * A client for making HTTP requests to an API with a base URL.
+ *
+ * @class HttpRequestClient
+ * @classdesc Provides functionality to make API calls with configurable endpoints and validation.
+ */
+export class HttpRequestClient {
+  /**
+   * Creates an instance of HttpRequestClient.
+   *
+   * @param {string} baseApiUrl - The base URL for all API requests.
+   */
+  constructor(private baseApiUrl: string) {}
+
+  /**
+   * Fetches data from an API endpoint with the provided configuration.
+   *
+   * @template T - The expected type of data to be returned from the API.
+   * @param {FetchAPIConfig<T>} config - The configuration for the API request.
+   * @param {string} config.endpoint - The API endpoint to fetch from, which will be appended to the base URL.
+   * @param {string} config.errorMessage - The error message to use if the request fails.
+   * @param {function(T): boolean} [config.validateResponse] - Optional function to validate the response data.
+   *
+   * @returns {Promise<T>} A promise that resolves to the data fetched from the API.
+   *
+   * @throws {Error} If the request fails, validation fails, or any other error occurs during the request.
+   */
+  async fetchApiWithConfig<T>(config: FetchAPIConfig<T>): Promise<T> {
+    const { endpoint, errorMessage, validateResponse } = config;
+
+    try {
+      const url = new URL(endpoint, this.baseApiUrl).toString();
+      const data = await fetchAPI<T>(url, config.errorMessage);
+
+      if (validateResponse && !validateResponse(data)) {
+        throw new Error(`${errorMessage}. Response validation failed for ${url}`);
+      }
+
+      return data;
+    } catch (error) {
+      throw new Error(`Failed to fetch from ${endpoint}: ${error}`);
+    }
+  }
+}
+
+/**
+ * Client interface for interacting with tasks and contests API endpoints.
+ *
+ * @template T - The type of parameters to pass to API methods, defaults to void if not specified.
+ */
+export interface TasksApiClient<T = void> {
+  getContests(params?: T): Promise<ContestsForImport>;
+  getTasks(params?: T): Promise<TasksForImport>;
+}
+
+/**
+ * @deprecated Use `HttpRequestClient` instead.
+ *
  * An abstract class representing a client for contest sites' APIs.
  * This class provides methods to fetch contests and tasks, and a protected method to fetch data from an API endpoint with a given configuration.
  *
