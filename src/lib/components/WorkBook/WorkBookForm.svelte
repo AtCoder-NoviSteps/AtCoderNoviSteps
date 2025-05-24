7f6aadf65435e57e9028ebe7be2a7184d5735292
Kato Hiroki <k.hiro1818@gmail.com>
2025-05-24 13:28:48 +0000

    ♻️ Extract common component (#2083)

---
+++ src/lib/components/WorkBook/WorkBookForm.svelte
@@ -0,0 +1,129 @@
+<script lang="ts">
+  import { run } from 'svelte/legacy';
+
+  import { Breadcrumb, BreadcrumbItem } from 'svelte-5-ui-lib';
+
+  import HeadingOne from '$lib/components/HeadingOne.svelte';
+  import WorkBookInputFields from '$lib/components/WorkBooks/WorkBookInputFields.svelte';
+  import WorkBookTasksTable from '$lib/components/WorkBookTasks/WorkBookTasksTable.svelte';
+  import TaskSearchBox from '$lib/components/TaskSearchBox.svelte';
+  import InputFieldWrapper from '$lib/components/InputFieldWrapper.svelte';
+  import SubmissionButton from '$lib/components/SubmissionButton.svelte';
+
+  import { preventEnterKey } from '$lib/actions/prevent_enter_key';
+
+  import type { Task, Tasks } from '$lib/types/task';
+  import type {
+    WorkBookTaskCreate,
+    WorkBookTasksCreate,
+    WorkBookTaskEdit,
+    WorkBookTasksEdit,
+  } from '$lib/types/workbook';
+
+  interface Props {
+    pageTitle: string;
+    breadcrumbTitle: string;
+    isAdmin: boolean;
+    // type is any, so we have no choice but to use it.
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    superFormObject: any; // superForm object
+    tasksMapByIds: Map<string, Task>;
+    submitButtonLabel?: string;
+  }
+
+  let {
+    pageTitle,
+    breadcrumbTitle,
+    isAdmin,
+    superFormObject,
+    tasksMapByIds,
+    submitButtonLabel = '',
+  }: Props = $props();
+
+  const { form, message, errors, enhance } = superFormObject;
+
+  let workBookTasksForTable: WorkBookTasksCreate | WorkBookTasksEdit = $state([]);
+
+  // HACK: This is a workaround to ensure that the derived state is updated correctly.
+  run(() => {
+    workBookTasksForTable = $form.workBookTasks
+      .map((workBookTask: WorkBookTaskCreate | WorkBookTaskEdit) => {
+        const task = tasksMapByIds.get(workBookTask.taskId);
+
+        if (!task) {
+          return null;
+        }
+
+        return {
+          contestId: task.contest_id,
+          title: task.title,
+          taskId: workBookTask.taskId,
+          priority: workBookTask.priority,
+          comment: workBookTask.comment,
+        };
+      })
+      .filter(
+        (item: WorkBookTaskCreate | WorkBookTaskEdit): item is NonNullable<typeof item> =>
+          item !== null,
+      );
+  });
+
+  const truncateClass = 'min-w-[96px] max-w-[120px] sm:max-w-[300px] lg:max-w-[600px] truncate';
+  const tasks: Tasks = $derived(Array.from(tasksMapByIds.values()));
+</script>
+
+<div class="container mx-auto w-5/6">
+  <form method="post" use:enhance use:preventEnterKey class="space-y-4">
+    <HeadingOne title={pageTitle} />
+
+    <Breadcrumb aria-label="">
+      <BreadcrumbItem href="/workbooks" home>問題集</BreadcrumbItem>
+
+      <BreadcrumbItem>
+        <div class={truncateClass}>{breadcrumbTitle}</div>
+      </BreadcrumbItem>
+    </Breadcrumb>
+
+    <!-- Form for workbook -->
+    <WorkBookInputFields
+      bind:authorId={$form.authorId}
+      bind:workBookTitle={$form.title}
+      bind:description={$form.description}
+      bind:editorialUrl={$form.editorialUrl}
+      bind:isPublished={$form.isPublished}
+      bind:isOfficial={$form.isOfficial}
+      bind:isReplenished={$form.isReplenished}
+      bind:workBookType={$form.workBookType}
+      {isAdmin}
+      message={$message}
+      errors={$errors}
+    />
+
+    <!-- Search tasks -->
+    <!-- HACK:
+      Because the attributes are slightly different, we have no choice
+      but to separate the data for storing in the database and for creating and editing workbooks.
+    -->
+    <div class="space-y-2">
+      <TaskSearchBox {tasks} bind:workBookTasks={$form.workBookTasks} bind:workBookTasksForTable />
+      <InputFieldWrapper
+        inputFieldType="hidden"
+        inputFieldName="workBookTasks"
+        inputValue={$form.workBookTasks}
+        message={$errors.workBookTasks?._errors}
+      />
+    </div>
+
+    <!-- Show tasks stored in database and added by search -->
+    <WorkBookTasksTable
+      {tasksMapByIds}
+      bind:workBookTasks={$form.workBookTasks}
+      bind:workBookTasksForTable
+    />
+
+    <!-- Create or update button  -->
+    <div class="flex flex-wrap md:justify-center md:items-center">
+      <SubmissionButton width="w-full md:max-w-md mt-4" labelName={submitButtonLabel} />
+    </div>
+  </form>
+</div>
--- src/routes/workbooks/create/+page.svelte
+++ src/routes/workbooks/create/+page.svelte
@@ -1,22 +1,10 @@
 <script lang="ts">
-  import { Breadcrumb, BreadcrumbItem } from 'svelte-5-ui-lib';
   import { superForm } from 'sveltekit-superforms/client';
 
-  import {
-    WorkBookType,
-    type WorkBookTasksBase,
-    type WorkBookTaskCreate,
-  } from '$lib/types/workbook';
+  import WorkBookForm from '$lib/components/WorkBook/WorkBookForm.svelte';
+
+  import { WorkBookType, type WorkBookTasksBase } from '$lib/types/workbook';
   import type { Task } from '$lib/types/task';
-
-  import { preventEnterKey } from '$lib/actions/prevent_enter_key';
-
-  import HeadingOne from '$lib/components/HeadingOne.svelte';
-  import WorkBookInputFields from '$lib/components/WorkBooks/WorkBookInputFields.svelte';
-  import WorkBookTasksTable from '$lib/components/WorkBookTasks/WorkBookTasksTable.svelte';
-  import TaskSearchBox from '$lib/components/TaskSearchBox.svelte';
-  import InputFieldWrapper from '$lib/components/InputFieldWrapper.svelte';
-  import SubmissionButton from '$lib/components/SubmissionButton.svelte';
 
   let { data } = $props();
 
@@ -26,75 +14,23 @@
     ...data.form,
     workBookTasks: [] as WorkBookTasksBase,
   };
-  const { form, message, errors, enhance } = superForm(initialData, {
+  const superFormObject = superForm(initialData, {
     dataType: 'json',
   });
+  const { form } = superFormObject;
 
   $form.authorId = data.author.id;
   $form.isOfficial = data.isAdmin;
   $form.workBookType = $form.isOfficial ? WorkBookType.CURRICULUM : WorkBookType.CREATED_BY_USER;
 
-  let workBookTasksForTable: WorkBookTaskCreate[] = $state([]);
-
-  $effect((): void => {
-    workBookTasksForTable = [] as WorkBookTaskCreate[];
-  });
-
   const tasksMapByIds: Map<string, Task> = data.tasksMapByIds;
 </script>
 
-<!-- TODO: 問題集の編集ページのコンポーネントとほぼ共通しているのでリファクタリング -->
-<div class="container mx-auto w-5/6">
-  <form method="post" use:enhance use:preventEnterKey class="space-y-4">
-    <HeadingOne title="問題集を作成" />
-
-    <Breadcrumb aria-label="">
-      <BreadcrumbItem href="/workbooks" home>問題集</BreadcrumbItem>
-      <BreadcrumbItem>
-        <div class="min-w-[96px] max-w-[120px] truncate">問題集を作成</div>
-      </BreadcrumbItem>
-    </Breadcrumb>
-
-    <WorkBookInputFields
-      bind:authorId={$form.authorId}
-      bind:workBookTitle={$form.title}
-      bind:description={$form.description}
-      bind:editorialUrl={$form.editorialUrl}
-      bind:isPublished={$form.isPublished}
-      bind:isOfficial={$form.isOfficial}
-      bind:isReplenished={$form.isReplenished}
-      bind:workBookType={$form.workBookType}
-      isAdmin={data.isAdmin}
-      message={$message}
-      errors={$errors}
-    />
-
-    <!-- 問題を検索 -->
-    <!-- HACK: 属性が微妙に異なるため、やむなくデータベースへの保存用と問題集作成・編集用で分けている。 -->
-    <div class="space-y-2">
-      <TaskSearchBox
-        tasks={Array.from(tasksMapByIds.values())}
-        bind:workBookTasks={$form.workBookTasks}
-        bind:workBookTasksForTable
-      />
-      <InputFieldWrapper
-        inputFieldType="hidden"
-        inputFieldName="workBookTasks"
-        inputValue={$form.workBookTasks}
-        message={$errors.workBookTasks?._errors}
-      />
-    </div>
-
-    <!-- 問題一覧 -->
-    <WorkBookTasksTable
-      {tasksMapByIds}
-      bind:workBookTasks={$form.workBookTasks}
-      bind:workBookTasksForTable
-    />
-
-    <!-- 作成ボタンを追加 -->
-    <div class="flex flex-wrap md:justify-center md:items-center">
-      <SubmissionButton width="w-full md:max-w-md mt-4" labelName="作成" />
-    </div>
-  </form>
-</div>
+<WorkBookForm
+  pageTitle="問題集を作成"
+  breadcrumbTitle={'問題集を作成'}
+  isAdmin={data.isAdmin}
+  {superFormObject}
+  {tasksMapByIds}
+  submitButtonLabel="作成"
+/>
