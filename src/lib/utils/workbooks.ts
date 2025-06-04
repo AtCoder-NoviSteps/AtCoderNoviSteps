d08a060a2dc2a7f7d20363a393692dcb8b560a51
Kato Hiroki <k.hiro1818@gmail.com>
2025-06-04 12:28:37 +0000

    ♻️ Fix format (#2020)

--- src/lib/utils/workbooks.ts
+++ src/lib/utils/workbooks.ts
@@ -1,6 +1,7 @@
 import { Roles } from '$lib/types/user';
-import { isAdmin } from '$lib/utils/authorship';
 import { type WorkbookList } from '$lib/types/workbook';
+
+import { isAdmin } from '$lib/utils/authorship';
 
 // 管理者 + ユーザ向けに公開されている場合
 export function canViewWorkBook(role: Roles, isPublished: boolean) {
