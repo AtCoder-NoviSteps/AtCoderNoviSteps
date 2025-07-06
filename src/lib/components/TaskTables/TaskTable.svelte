5fc8eb8b85050db5ee1eed30319e96dbfd2e9a8d
Kato Hiroki <k.hiro1818@gmail.com>
2025-07-06 06:34:01 +0000

    :art: Fix layout (#1956)

--- src/lib/components/TaskTables/TaskTable.svelte
+++ src/lib/components/TaskTables/TaskTable.svelte
@@ -102,7 +102,7 @@
 
   function getBodyCellClasses(taskResult: TaskResult, totalColumns: number): string {
     const baseClasses = 'w-1/2 xs:w-1/3 sm:w-1/4 md:w-1/5 lg:w-1/6 px-1 py-1';
-    const additionalClasses = totalColumns > 8 ? 'lg:w-1/7 2xl:w-1/8' : '';
+    const additionalClasses = totalColumns > 8 ? 'lg:w-1/7 2xl:w-1/8 py-2' : '';
     const backgroundColor = getBackgroundColor(taskResult);
 
     return `${baseClasses} ${additionalClasses} ${backgroundColor}`;
@@ -186,7 +186,7 @@
     {metadata.title}
   </Heading>
 
-  <div class="container w-full rounded-md border shadow-sm">
+  <div class="container w-full rounded-md border shadow-sm mb-6">
     <!-- Table header -->
     <!-- TODO: 複数のコンテストを1ページで表示するときはoffにする -->
     <div class="w-full sticky top-0 z-20 border-b">
