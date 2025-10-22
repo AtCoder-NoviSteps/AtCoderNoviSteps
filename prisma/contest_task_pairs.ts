8fce9458d5cef5b1637510de4020f5dfc14ed59c
Kato Hiroki <k.hiro1818@gmail.com>
2025-10-22 13:28:51 +0000

    feat: Add contest task pairs to seeds (#2734)

---
+++ prisma/contest_task_pairs.ts
@@ -0,0 +1,67 @@
+export const contest_task_pairs = [
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'typical90_s',
+    problem_index: 'C18',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'math_and_algorithm_ac',
+    problem_index: 'C09',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'abc007_3',
+    problem_index: 'B63',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'math_and_algorithm_ap',
+    problem_index: 'B28',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'dp_a',
+    problem_index: 'B16',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'math_and_algorithm_al',
+    problem_index: 'B07',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'typical90_a',
+    problem_index: 'A77',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'math_and_algorithm_an',
+    problem_index: 'A63',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'math_and_algorithm_am',
+    problem_index: 'A62',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'math_and_algorithm_bn',
+    problem_index: 'A39',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'math_and_algorithm_aq',
+    problem_index: 'A29',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'math_and_algorithm_o',
+    problem_index: 'A27',
+  },
+  {
+    contest_id: 'tessoku-book',
+    problem_id: 'math_and_algorithm_ai',
+    problem_index: 'A06',
+  },
+];
