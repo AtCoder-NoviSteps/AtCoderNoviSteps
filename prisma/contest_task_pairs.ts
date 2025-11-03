c7752dee38e3797822a20d5aed491198f66c0082
Kato Hiroki <k.hiro1818@gmail.com>
2025-11-03 10:00:33 +0000

    feat: Add seeds for math and algo book (#2785)

--- prisma/contest_task_pairs.ts
+++ prisma/contest_task_pairs.ts
@@ -64,4 +64,139 @@ export const contest_task_pairs = [
     problem_id: 'math_and_algorithm_ai',
     problem_index: 'A06',
   },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'arc117_c',
+    problem_index: '102',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'typical90_o',
+    problem_index: '101',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'typical90_am',
+    problem_index: '099',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'abc204_d',
+    problem_index: '096',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'typical90_j',
+    problem_index: '095',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'abc140_c',
+    problem_index: '094',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'typical90_al',
+    problem_index: '093',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'typical90_y',
+    problem_index: '090',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'arc107_a',
+    problem_index: '088',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'panasonic2020_c',
+    problem_index: '084',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'typical90_n',
+    problem_index: '083',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'abc139_d',
+    problem_index: '079',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'abc186_d',
+    problem_index: '076',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'jsc2021_c',
+    problem_index: '072',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'abc075_d',
+    problem_index: '070',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'abc178_b',
+    problem_index: '069',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'typical90_d',
+    problem_index: '067',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'panasonic2020_b',
+    problem_index: '065',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'abc167_d',
+    problem_index: '062',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'abc145_d',
+    problem_index: '052',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'arc084_b',
+    problem_index: '048',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'abc007_3',
+    problem_index: '046',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'typical90_bz',
+    problem_index: '045',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'abc172_d',
+    problem_index: '042',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'abc168_c',
+    problem_index: '036',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'dp_d',
+    problem_index: '030',
+  },
+  {
+    contest_id: 'math-and-algorithm',
+    problem_id: 'dp_a',
+    problem_index: '028',
+  },
 ];
