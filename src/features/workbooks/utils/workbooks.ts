787960679ee73e038b2a6d7e43bbd279797b109d
Kato Hiroki <k.hiro1818@gmail.com>
2026-03-01 07:19:14 +0000

    refactor: Extract calcWorkBookGradeModes to features/utils (#943)

--- src/features/workbooks/utils/workbooks.ts
+++ src/features/workbooks/utils/workbooks.ts
@@ -1,7 +1,14 @@
 import { Roles } from '$lib/types/user';
-import type { WorkBook, WorkbookList } from '$features/workbooks/types/workbook';
+import { TaskGrade, type Task, type TaskGrades } from '$lib/types/task';
+import type {
+  WorkBook,
+  WorkbookList,
+  WorkbooksList,
+  WorkBookTaskBase,
+} from '$features/workbooks/types/workbook';
 
 import { isAdmin } from '$lib/utils/authorship';
+import { calcGradeMode } from '$lib/utils/task';
 
 // 管理者 + ユーザ向けに公開されている場合
 export function canViewWorkBook(role: Roles, isPublished: boolean) {
@@ -18,4 +25,39 @@ export function getUrlSlugFrom(workbook: WorkbookList | WorkBook): string {
   const slug = workbook.urlSlug;
 
   return slug ? slug : workbook.id.toString();
+}
+
+/**
+ * Calculates the grade modes for a list of workbooks in curriculum based on their tasks.
+ *
+ * @param workbooks - The list of workbooks for curriculum, each containing an array of tasks with their IDs and priorities
+ * @param tasksByTaskId - A map of task IDs to task objects
+ *
+ * @returns A map of workbook IDs to their corresponding grade modes
+ * @note The time complexity is O(N * M * log(M)), where N is the number of workbooks and M is the average number of tasks per workbook.
+ */
+export function calcWorkBookGradeModes(
+  workbooks: WorkbooksList,
+  tasksByTaskId: Map<string, Task>,
+): Map<number, TaskGrade> {
+  const gradeModes: Map<number, TaskGrade> = new Map();
+
+  workbooks.forEach((workbook: WorkbookList) => {
+    const taskGrades = workbook.workBookTasks.reduce(
+      (results: TaskGrades, workBookTask: WorkBookTaskBase) => {
+        const task = tasksByTaskId.get(workBookTask.taskId);
+
+        if (task && task.grade !== TaskGrade.PENDING) {
+          results.push(task.grade as TaskGrade);
+        }
+
+        return results;
+      },
+      [],
+    );
+
+    gradeModes.set(workbook.id, calcGradeMode(taskGrades));
+  });
+
+  return gradeModes;
 }
