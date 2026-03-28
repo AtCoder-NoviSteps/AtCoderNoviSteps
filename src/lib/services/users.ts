a8beb67faf50bd47864e0bfcf73071541bb76e1f
river0525 <0525sotaro@gmail.com>
2026-03-28 09:29:47 +0900

    refactor: update users.ts and hooks.server.ts to use AtCoderAccount relation

--- src/lib/services/users.ts
+++ src/lib/services/users.ts
@@ -1,52 +1,19 @@
 import { default as db } from '$lib/server/database';
-import type { User } from '@prisma/client';
 
 export async function getUser(username: string) {
-  const user = await db.user.findUnique({
-    where: {
-      username: username,
-    },
+  return await db.user.findUnique({
+    where: { username },
+    include: { atCoderAccount: true },
   });
-  return user;
 }
 
 export async function getUserById(userId: string) {
-  const user = await db.user.findUnique({
-    where: {
-      id: userId,
-    },
+  return await db.user.findUnique({
+    where: { id: userId },
+    include: { atCoderAccount: true },
   });
-  return user;
 }
 
 export async function deleteUser(username: string) {
-  const user = await db.user.delete({
-    where: {
-      username: username,
-    },
-  });
-  return user;
-}
-
-export async function updateValicationCode(
-  username: string,
-  atcoder_id: string,
-  validationCode: string,
-) {
-  try {
-    const user: User | null = await db.user.update({
-      where: {
-        username: username,
-      },
-
-      data: {
-        atcoder_validation_code: validationCode,
-        atcoder_username: atcoder_id,
-      },
-    });
-
-    return user;
-  } catch {
-    console.log('user update error');
-  }
+  return await db.user.delete({ where: { username } });
 }
