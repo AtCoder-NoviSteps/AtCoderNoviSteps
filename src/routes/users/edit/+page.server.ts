4d391c126913f7053f1a08a4d2f22905aaa34a3c
Kato Hiroki <k.hiro1818@gmail.com>
2026-03-29 08:52:05 +0000

    chore: Fix format

--- src/routes/users/edit/+page.server.ts
+++ src/routes/users/edit/+page.server.ts
@@ -1,15 +1,18 @@
 //See https://tech-blog.rakus.co.jp/entry/20230209/sveltekit#%E3%82%B9%E3%83%AC%E3%83%83%E3%83%89%E6%8A%95%E7%A8%BF%E7%94%BB%E9%9D%A2
 
+import { redirect, fail } from '@sveltejs/kit';
+
+import type { Actions } from './$types';
 import type { Roles } from '$lib/types/user';
+
 import * as userService from '$lib/services/users';
 import * as verificationService from '$features/account/services/atcoder_verification';
-import type { Actions } from './$types';
 
-import { redirect, fail } from '@sveltejs/kit';
 import { BAD_REQUEST, FORBIDDEN } from '$lib/constants/http-response-status-codes';
 
 export async function load({ locals, url }) {
   const session = await locals.auth.validate();
+
   if (!session) {
     redirect(302, '/login');
   }
