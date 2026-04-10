49f697647573ec53f898bc669d5c6f27f6f302d6
Kato Hiroki <k.hiro1818@gmail.com>
2026-04-10 22:53:06 +0000

    chore: remove and add external links (#3395)

--- src/lib/constants/external-links.ts
+++ src/lib/constants/external-links.ts
@@ -3,7 +3,8 @@ import {
   ATCODER_PROBLEMS_URL,
   AOJ_ATCODER_JOI_URL,
   AOJ_PCK_URL,
-  ICPC_JAPAN_PROBLEMS_URL,
+  X_URL,
+  MOND_URL,
   GITHUB_URL,
 } from './urls';
 
@@ -12,6 +13,7 @@ export const externalLinks = [
   { title: `AtCoder Problems`, path: ATCODER_PROBLEMS_URL },
   { title: `AOJ / AtCoder-JOI`, path: AOJ_ATCODER_JOI_URL },
   { title: `AOJ-PCK`, path: AOJ_PCK_URL },
-  { title: `ICPC Japan Problems`, path: ICPC_JAPAN_PROBLEMS_URL },
+  { title: `X`, path: X_URL },
+  { title: `mond`, path: MOND_URL },
   { title: `GitHub`, path: GITHUB_URL },
 ];
